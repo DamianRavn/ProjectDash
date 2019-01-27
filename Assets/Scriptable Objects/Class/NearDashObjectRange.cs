da639e7d2fc7f141d0ea9a3f747e7a2400b9887e
Damian Ravn <DamianRavn@gmail.com>
2019-01-27 18:29:22 +0100

    implemented enteties to adjust range indicator when player is near. Changed force to 10

---
+++ Assets/Scriptable Objects/Class/NearDashObjectRange.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NearDashObjectRange", menuName = "ScriptableObjects/NearDashObjectRange")]
+public class NearDashObjectRange : ScriptableObject
+{
+    public uint range;
+}
---
+++ Assets/Scripts/ECS/NearDashObjectRenderRange.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class NearDashObjectRenderRange : MonoBehaviour
+{
+    public Transform player;
+    public NearDashObjectRange rangeObject;
+}
+
+class NearDashRangeBehavior : ComponentSystem
+{
+    struct Components
+    {
+        public NearDashObjectRenderRange nearDash;
+        public SpriteRenderer spriteRendere;
+    }
+
+    protected override void OnUpdate()
+    {
+        foreach (var e in GetEntities<Components>())
+        {
+            var dist = Vector3.Distance(e.nearDash.player.position, e.nearDash.transform.position);
+            if (dist < e.nearDash.rangeObject.range)
+            {
+                var colortmp = e.spriteRendere.color;
+                colortmp.a = ExtensionMethods.Remap(dist, 0, e.nearDash.rangeObject.range, 0.3f, 0);
+                e.spriteRendere.color = colortmp;
+            }
+            else
+            {
+                var colortmp = e.spriteRendere.color;
+                colortmp.a = 0;
+                e.spriteRendere.color = colortmp;
+            }
+        }
+    }
+}

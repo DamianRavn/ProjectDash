1f5a5dcf772efb8b4ac8968587d8420aa46c1cca
Damian Ravn <DamianRavn@gmail.com>
2019-02-11 21:36:38 +0100

    First itteration
    Cleaned up ecs, getter and setter. Made Dash script.

--- Assets/Scripts/ECS/NearDashObjectRenderRange.cs
+++ Assets/Scripts/ECS/NearDashObjectRenderRange.cs
@@ -1,33 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Entities;
+
 
 public class NearDashObjectRenderRange : MonoBehaviour
 {
     public Transform player;
     public NearDashObjectRange rangeObject;
-}
-
-class NearDashRangeBehavior : ComponentSystem
-{
-    struct Components
-    {
-        public NearDashObjectRenderRange nearDash;
-        public SpriteRenderer spriteRendere;
-    }
-
-    protected override void OnUpdate()
-    {
-        foreach (var e in GetEntities<Components>())
-        {
-            var dist = Vector3.Distance(e.nearDash.player.position, e.nearDash.transform.position);
-            if (dist < e.nearDash.rangeObject.range)
-            {
-                var colortmp = e.spriteRendere.color;
-                colortmp.a = ExtensionMethods.Remap(dist, 0, e.nearDash.rangeObject.range -0.4f, 0.3f, 0);
-                e.spriteRendere.color = colortmp;
-            }
-        }
-    }
 }
